4f45ec0b9da5202e04a50557afb7862579b9c9f9
MaximeJeannette <102291390+MaximeJeannette@users.noreply.github.com>
2023-05-04 16:25:12 +0200

    misc: test api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(`http://www.omdbapi.com/?apikey=23aaa32&t=transfor`)
+      .then((response) => response.json())
+      .then((actualData) => console.log(actualData))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return <Typography variant="h1">Hello World !</Typography>;
 }
 
