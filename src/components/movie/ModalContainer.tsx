4fc11cbdd0bfee5dd1d4c06af493f0f63e992adf
MaximeJeannette <102291390+MaximeJeannette@users.noreply.github.com>
2023-05-06 00:55:45 +0200

    feat: handle movie details data

--- src/components/movie/ModalContainer.tsx
+++ src/components/movie/ModalContainer.tsx
@@ -8,10 +8,9 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  // width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
+  boxShadow: 20,
   p: 4,
 };
 
@@ -31,25 +30,65 @@ const ModalContainer = () => {
 
   if (loading) {
     return (
-      <Box sx={{ display: "flex", marginTop: "35px" }}>
-        <CircularProgress />
+      <Box sx={style}>
+        <Box sx={{ display: "flex", marginTop: "35px" }}>
+          <CircularProgress />
+        </Box>
       </Box>
     );
   }
 
   if (errorMsg) {
     return (
-      <Typography sx={{ marginTop: "35px" }} variant="h4" color="error.main">
-        {errorMsg}
-      </Typography>
+      <Box sx={style}>
+        <Typography sx={{ marginTop: "35px" }} variant="h4" color="error.main">
+          {errorMsg}
+        </Typography>
+      </Box>
     );
   }
 
-  console.log("data: ", data);
-
   return (
-    <Modal open={modal} onClose={() => dispatch(setModalStatus(false))}>
-      <Box sx={style}>bonjour</Box>
+    <Modal
+      open={modal}
+      onClose={() => {
+        dispatch(setModalStatus(false));
+      }}
+    >
+      <Box sx={style}>
+        <Box sx={{ display: "flex" }}>
+          <img
+            src={data.Poster}
+            alt={`${data.Title} poster`}
+            style={{ width: "200px", height: "auto%", borderRadius: "4px" }}
+          />
+          <Box sx={{ marginLeft: "20px" }}>
+            <Typography variant="h5">{data.Title}</Typography>
+            <Typography variant="body1">{data.Director}</Typography>
+            <Typography variant="body1">{data.Actors}</Typography>
+            <Typography variant="body2">{data.Released}</Typography>
+            <Typography variant="body2">{data.Runtime}</Typography>
+            <Typography variant="body2">{data.Genre}</Typography>
+            <Typography variant="body2">{data.Language}</Typography>
+            <Typography variant="body2">{data.Rated}</Typography>
+            <Typography variant="body2">
+              Metascore : {data.Metascore}
+            </Typography>
+            <Typography variant="body2">{data.BoxOffice}</Typography>
+            <Typography variant="body2">
+              Box office : {data.BoxOffice}
+            </Typography>
+            <Typography variant="body2">DVD: {data.DVD}</Typography>
+          </Box>
+        </Box>
+
+        <Typography variant="body1" sx={{ marginTop: "20px" }}>
+          Resume :
+        </Typography>
+        <Typography variant="caption" sx={{ marginTop: "20px" }}>
+          {data.Plot}
+        </Typography>
+      </Box>
     </Modal>
   );
 };
