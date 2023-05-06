ff512eb625354486f41407033499568bfec9a4a5
MaximeJeannette <102291390+MaximeJeannette@users.noreply.github.com>
2023-05-06 03:03:08 +0200

    feat: jest tests validate

--- src/MoviePicker/MoviePicker.ts
+++ src/MoviePicker/MoviePicker.ts
@@ -4,6 +4,20 @@
 
 import { MoviePickRepo } from "./MoviePickRepo";
 
+export class MoviePickAlreadyExistError extends Error {
+  constructor(err: string) {
+    super(err);
+    console.log("err: ", err);
+  }
+}
+
+export class EmptyMovieTitleError extends Error {
+  constructor(err: string) {
+    super(err);
+    console.log("empty Movie Title: ", err);
+  }
+}
+
 export class MoviePicker {
   moviePickRepo: MoviePickRepo;
 
@@ -11,5 +25,21 @@ export class MoviePicker {
     this.moviePickRepo = moviePickRepo;
   }
 
-  console.log(mov)
+  async pick(title: string) {
+    const alreadyExist = await this.moviePickRepo.getByFirstLetter(
+      title.charAt(0).toUpperCase()
+    );
+
+    if (!title.length) {
+      throw new EmptyMovieTitleError("Empty title");
+    }
+
+    if (title.length > 0 && alreadyExist !== null) {
+      throw new MoviePickAlreadyExistError("This letter has already a pick");
+    }
+
+    if (title.length > 0 && alreadyExist === null) {
+      this.moviePickRepo.put(title);
+    }
+  }
 }
